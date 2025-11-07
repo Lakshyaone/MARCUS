f58f1d701c5882ea79b4922282950c29d1b5b8c8
Lakshya <Lakshya8998@gmail.com>
2025-11-07 10:24:55 +0530

    changes

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-import google.generativeai as genai
-
-load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("models/gemini-2.5-flash")
-response = model.generate_content("Hello, how are you?")
-print(response.text)
